21cda11489a51c0016a32c0a3a8596c78cb5d800
unknown <mathias.muench@mathias-muench.de>
2014-09-26 15:47:58 +0200

    tomcat logging

---
+++ tomcat-logging/src/main/java/de/mathias_muench/tomcat_logging/HelloBean.java
@@ -0,0 +1,13 @@
+package de.mathias_muench.tomcat_logging;
+
+public class HelloBean {
+	private String input;
+
+	public String getInput() {
+		return input;
+	}
+
+	public void setInput(String input) {
+		this.input = input;
+	}
+}
