47f6d81ae3d0d94a51d6e68ae5b16f152847fec5
unknown <mathias.muench@mathias-muench.de>
2014-10-24 10:02:38 +0200

    Programmatisch konfigurieren

--- MyApp/src/main/java/MyApp.java
+++ MyApp/src/main/java/MyApp.java
@@ -1,8 +1,17 @@
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+
 import com.tngtech.mmu.logging.app.Database;
 
 public class MyApp {
+	private static final Logger LOG = Logger.getLogger(MyApp.class);
+
 	public static void main(String[] args) throws Throwable {
+		System.out.println(Logger.getRootLogger().getAllAppenders().hasMoreElements());
+		System.out.println(LOG.getAllAppenders().hasMoreElements());
+		BasicConfigurator.configure();
 		Database database = new Database();
+		LOG.info(database.toString());
 		database.write(args[0]);
 	}
 }
